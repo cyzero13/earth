2e9a0b1ccdb952e2ea1319bb2551e5eeadbeb4da
Deony Diras <deony.diras1998@gmai.com>
2026-03-14 17:51:23 +0800

    Updates

---
+++ _next/static/fOWHtnJZCBv1z85AwYVCa/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/earth//_next/:path+",
+        "destination": "/earth/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
