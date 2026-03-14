e3b358da20f10886410657e6088a3cdf3d0743ad
Deony Diras <deony.diras1998@gmai.com>
2026-03-14 17:18:35 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,59 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+const inter = localFont({
+  src: [
+    {
+      path: "../public/fonts/Inter/InterVariable.woff2",
+      weight: "100 900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter/InterVariable-Italic.woff2",
+      weight: "100 900",
+      style: "italic",
+    },
+  ],
+  variable: "--inter",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const appleGaramond = localFont({
+  src: [
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond-LightItalic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/AppleGaramond/AppleGaramond-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--apple-garamond",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +68,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body className={`${inter.variable} ${appleGaramond.variable}`}>
+          <main>
+            {children}
+          </main>
+        </body>
     </html>
   );
 }
